ae65579a1c3974252556ee5d71f6592aec0aaca6
Bruno Alencar <bruno.idealencar@gmail.com>
2017-06-19 09:55:51 -0300

    First commit/Create server and clients communication

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using Reverse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Threading;
+using System.Reactive.Linq;
+using System.Collections.ObjectModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var id = "";
+            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            
+            var client = new ReverseService.ReverseServiceClient(channel);
+
+            var chat = client.chat();
+
+            Task.Run(async () =>
+            {
+                while (await chat.ResponseStream.MoveNext())
+                {
+                    var msg = chat.ResponseStream.Current.Message;
+                    Console.WriteLine($"{msg.From}: {msg.Message}");
+                }
+            });
+
+            Console.Write("Set name: ");
+            id = Console.ReadLine();
+
+            while (true)
+            {
+                var msg = Console.ReadLine();
+
+                if (msg == "bye")
+                    break;
+
+                chat.RequestStream.WriteAsync(new ChatMessage
+                {
+                    From = id,
+                    Message = msg
+                }).ContinueWith(t => { });
+            }
+
+            channel.ShutdownAsync().Wait();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,60 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reverse;
+
+namespace server
+{
+    class GreeterImpl : Reverse.ReverseService.ReverseServiceBase
+    {
+        private List<IServerStreamWriter<ChatMessageFromServer>> clients;
+        public GreeterImpl()
+        {
+
+            clients = new List<IServerStreamWriter<ChatMessageFromServer>>();
+        }
+
+        public override Task<ReverseReply> ReverseString(ReverseRequest request, ServerCallContext context)
+        {
+
+            Console.WriteLine($"Received from client {request.Data}");
+            return Task.FromResult(new ReverseReply { Reversed = request.Data });
+        }
+        public override async Task chat(IAsyncStreamReader<ChatMessage> requestStream, IServerStreamWriter<ChatMessageFromServer> responseStream, ServerCallContext context)
+        {
+            clients.Add(responseStream);
+
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+                foreach (var client in clients)
+                    await client.WriteAsync(new ChatMessageFromServer() { Message = message });
+            }
+
+        }
+    }
+
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { ReverseService.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
